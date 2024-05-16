5e7f4388b8d396c0a81066191002f0746ef01392
marywam <maryannewambui0124@gmail.com>
2024-05-16 15:19:33 +0300

    Sign-in

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import { useState } from "react";
 const SignIn = () => {
-  return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
-  )
-}
+  const [form, setform] = useState({
+    email: "",
+    password: "",
+  });
 
-export default SignIn
+  return (
+    <SafeAreaView className="bg-primary h-full ">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h=[35px]"
+          />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setform({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setform({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
