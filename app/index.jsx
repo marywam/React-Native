4f2a07091e31a24c17daf2b1bd1c80bc425d6183
marywam <maryannewambui0124@gmail.com>
2024-05-16 12:36:14 +0300

    update page

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import {  Text,  View } from 'react-native';
-import { Link } from 'expo-router';
+
+import {  ScrollView, Image, View, Text } from 'react-native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-plight">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: 'red'}} >Go To Home</Link>
+    <SafeAreaView className="bg-primary h-full">
+       <ScrollView contentContainerStyle={{height: '100%'}}>
+          <View className="w-full justify-center items-center h-full px-4">
+              <Image
+                source={images.logo}
+                className="w-[100px] h-[84px]"
+                resizeMode='contain'
+              />
 
-          
-    </View>
+              <Image
+                 source={images.cards}
+                 className="max-w--[380px] w-full h-[300px]"
+                 resizeMode='contain'
+              />
+              <View className="relative mt-5">
+                 <Text className="text-3xl text-white font-bold text-center">Discover with {''}<Text className="text-secondary-200">Aora</Text></Text>
+                 <Image
+                   source={images.path}
+                   className="w-[146px] h-[15px] absolute -bottom-2 -right-8"
+                   resizeMode='contain'
+                 />
+              </View>
+              <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation:
+                embark on a journey of limited exploration
+              </Text>
+          </View>
+       </ScrollView>
+    </SafeAreaView>
   );
 }
